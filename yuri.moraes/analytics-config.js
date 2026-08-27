e77a782868413ea7af1fd9f457ceb67651b1f77d
Mariana Assistente <yuri.moraes@stmgroup.com.br>
2026-08-27 12:23:36 +0000

    feat: add privacy-first analytics preview

---
+++ yuri.moraes/analytics-config.js
@@ -0,0 +1,10 @@
+window.__MANIFESTO_ANALYTICS_CONFIG__ = Object.freeze({
+  mode: 'preview',
+  build: 'manifesto-yuri-analytics-preview-20260827',
+  consentVersion: '2026-08-27',
+  cloudflareToken: '',
+  posthogKey: '',
+  posthogHost: 'https://us.i.posthog.com',
+  replaySampleRate: 0,
+  debug: true
+});
